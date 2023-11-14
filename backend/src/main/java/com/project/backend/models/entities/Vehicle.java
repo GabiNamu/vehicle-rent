4f1831e1ead7e2c5f14006ff4c49d3e2ac230687
Gabriela Muniz <gabrielanamu@live.com>
2023-11-14 10:13:49 -0300

    feat: add Vehicle entity class

---
+++ backend/src/main/java/com/project/backend/models/entities/Vehicle.java
@@ -0,0 +1,86 @@
+package com.project.backend.models.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+/**
+ * The type Vehicle.
+ */
+@Entity
+public class Vehicle {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int id;
+  private String model;
+  private String brand;
+  private String color;
+  private int manufacturingYear;
+  private String licensePlate;
+
+  /**
+   * Instantiates a new Vehicle.
+   */
+  public Vehicle() {
+
+  }
+
+  /**
+   * Instantiates a new Vehicle.
+   *
+   * @param model             the model
+   * @param brand             the brand
+   * @param color             the color
+   * @param manufacturingYear the manufacturing year
+   * @param licensePlate      the license plate
+   */
+  public Vehicle(String model, String brand, String color, int manufacturingYear, String licensePlate) {
+    this.model = model;
+    this.brand = brand;
+    this.color = color;
+    this.manufacturingYear = manufacturingYear;
+    this.licensePlate = licensePlate;
+  }
+
+  public String getModel() {
+    return model;
+  }
+
+  public void setModel(String model) {
+    this.model = model;
+  }
+
+  public String getBrand() {
+    return brand;
+  }
+
+  public void setBrand(String brand) {
+    this.brand = brand;
+  }
+
+  public String getColor() {
+    return color;
+  }
+
+  public void setColor(String color) {
+    this.color = color;
+  }
+
+  public int getManufacturingYear() {
+    return manufacturingYear;
+  }
+
+  public void setManufacturingYear(int manufacturingYear) {
+    this.manufacturingYear = manufacturingYear;
+  }
+
+  public String getLicensePlate() {
+    return licensePlate;
+  }
+
+  public void setLicensePlate(String licensePlate) {
+    this.licensePlate = licensePlate;
+  }
+}
