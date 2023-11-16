7dfdf875bb5df9aae219efc9b607db7453f4bc46
Gabriela Muniz <gabrielanamu@live.com>
2023-11-16 19:13:07 -0300

    fix: remove scss file path from app component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,8 +3,7 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   constructor(private modalService: NgbModal) {
